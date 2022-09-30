fd56ec94b381ee09408f83ed1f23d52f1035d677
Deniz Eryilmaz <deniz@heroconcept.com>
2022-10-01 02:40:07 +0300

    Upgrade System, URP Render Pipeline

---
+++ Assets/Scripts/Upgrade/Upgrade.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    public static Upgrade Instance {get; private set;}
+    const int startUpgradeSize = 1;
+    private int currentUpgradeOrbCount;
+    private int upgradeMilestone;
+    private int upgradeLevel;
+
+    void Awake() {
+      if (Instance != null && Instance != this) {
+        Destroy(this);
+      }
+      else {
+        Instance = this;
+        Upgrade.Instance.init();
+      } 
+    }
+    void init()
+    {
+      currentUpgradeOrbCount = startUpgradeSize;
+      upgradeMilestone = 5;
+      upgradeLevel = 0;
+    }
+
+
+    public void getUpgradeOrb(int getSize) 
+    {
+      currentUpgradeOrbCount += getSize;
+      checkUpdate();
+    }
+
+    private void checkUpdate() 
+    {
+      if(currentUpgradeOrbCount == upgradeMilestone) {
+        nextLevel();
+      }
+      else if(currentUpgradeOrbCount < 0) {
+        previousLevel();
+      }
+    }
+    void nextLevel()
+    {
+      upgradeMilestone += 1;
+      currentUpgradeOrbCount = 0;
+      upgradeLevel += 1;
+    }
+    void previousLevel()
+    {
+      upgradeMilestone -= 1;
+      currentUpgradeOrbCount = upgradeMilestone - 1;
+      upgradeLevel -= 1;
+    }
+
+    private void Update() 
+    {
+      if(Input.GetKeyDown(KeyCode.A)) {
+        getUpgradeOrb(-1);
+      }
+
+      if(Input.GetKeyDown(KeyCode.D)) {
+        getUpgradeOrb(+1);
+      } 
+    }
+    private void OnGUI() {
+      GUI.Label(new Rect(5,5,300,600), $"{currentUpgradeOrbCount.ToString()}/{upgradeMilestone.ToString()}");
+      GUI.Label(new Rect(5,20,300,600), $"{upgradeLevel.ToString()}");
+    }
+
+
+}
